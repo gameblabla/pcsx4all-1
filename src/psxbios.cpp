1a4b0ee4e521965d8ab0632bccf1007799d814ab
gameblabla <gameblabla@openmailbox.org>
2019-02-13 08:06:40 +0100

    psxbios: Better realloc implementation
    This should be closer to the real behaviour as described by nocash.
    It doesn't do any bcopy though but it shouldn't be too much different
    other than that.

--- src/psxbios.cpp
+++ src/psxbios.cpp
@@ -1200,9 +1200,24 @@ void psxBios_realloc(void) { // 38
 #endif
 
 	a0 = block;
-	psxBios_free();
-	a0 = size;
-	psxBios_malloc();
+	/* If "old_buf" is zero, executes malloc(new_size), and returns r2=new_buf (or 0=failed). */
+	if (block == 0)
+	{
+		psxBios_malloc();
+	}
+	/* Else, if "new_size" is zero, executes free(old_buf), and returns r2=garbage. */
+	else if (size == 0)
+	{
+		psxBios_free();
+	}
+	/* Else, executes malloc(new_size), bcopy(old_buf,new_buf,new_size), and free(old_buf), and returns r2=new_buf (or 0=failed). */
+	/* Note that it is not quite implemented this way here. */
+	else
+	{
+		psxBios_free();
+		a0 = size;
+		psxBios_malloc();
+	}
 }
 
 
